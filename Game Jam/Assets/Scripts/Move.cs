c6b46a8d9bb659a6de2e512707dd5993e7f229fb
RavinderrajSingh <79821708+RavinderrajSingh@users.noreply.github.com>
2022-08-21 18:04:27 +0530

    /

--- Game Jam/Assets/Scripts/Move.cs
+++ Game Jam/Assets/Scripts/Move.cs
@@ -1,8 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections;
 using UnityEngine;
 
 public class Move : MonoBehaviour
 {
-    
+    InputManager input;
+    Rigidbody2D rb;
+
+    private void Awake()
+    {
+        input = InputManager.Instance;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+
 }
