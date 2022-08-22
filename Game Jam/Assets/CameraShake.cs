07a9aa44152b208c69cf2bd9b32b16957bdda32f
RavinderrajSingh <79821708+RavinderrajSingh@users.noreply.github.com>
2022-08-22 16:20:55 +0530

    /

---
+++ Game Jam/Assets/CameraShake.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraShake : MonoBehaviour
+{
+    [SerializeField] CinemachineImpulseSource screenShake;
+    [SerializeField] float powerAmount;
+
+    public void ShakeScreen()
+    {
+        screenShake.GenerateImpulse();
+    }
+}
