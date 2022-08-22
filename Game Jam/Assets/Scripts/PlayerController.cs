55c3ed83def845d06927b7ae76a5a6b87456eae8
RavinderrajSingh <79821708+RavinderrajSingh@users.noreply.github.com>
2022-08-22 12:52:30 +0530

    /

---
+++ Game Jam/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    PlayerManager manager;
+
+    private void Awake()
+    {
+        manager = GetComponent<PlayerManager>();
+        print(manager.motor_Script.transform.name);
+        print(manager.jump_Script.transform.name);
+        print(manager.dash_Script.transform.name);
+    }
+}
--- Game Jam/Assets/Scripts/PlayerManager.cs
+++ Game Jam/Assets/Scripts/PlayerManager.cs
@@ -4,6 +4,45 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
-    [SerializeField] PlayerTypeSO settings;
-    
+    public PlayerTypeSO settings;
+    public InputManager inputManager;
+    public Motor motor_Script;
+    public Jump jump_Script;
+    public Dash dash_Script;
+
+    private void Awake()
+    {
+        if(settings.playerType == PlayerTypeSO.PlayerType.Player)
+        {
+            TryGetPlayerFiles();
+            if (motor_Script != null) motor_Script.enabled = true;
+            if (jump_Script != null) jump_Script.enabled = true;
+            if (dash_Script != null) dash_Script.enabled = true;
+        }
+        else if(settings.playerType == PlayerTypeSO.PlayerType.Shooter)
+        {
+            // TODO
+        }
+
+        inputManager = InputManager.Instance;
+    }
+
+
+    private void TryGetPlayerFiles()
+    {
+        if (TryGetComponent<Motor>(out Motor motor)) motor_Script = GetComponent<Motor>(); 
+        if (TryGetComponent<Jump>(out Jump jump)) jump_Script = GetComponent<Jump>(); 
+        if (TryGetComponent<Dash>(out Dash dash)) dash_Script = GetComponent<Dash>(); 
+    }
+
+    private void TryDisableShooterFiles()
+    {
+        // TODO
+    }
+
+    private void TryGetShooterFiles()
+    {
+        // TODOD
+    }
+
 }
